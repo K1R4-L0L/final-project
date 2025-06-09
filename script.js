45b5504986b9f69cd9b017fb299b2f4b20015ebd
yadiel <yadield2889@tuckahoeschools.org>
2025-06-09 12:31:06 +0000

    code is so buns

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 const streak = document.querySelector(".score p");
+const high = document.querySelector("Higher")
+const low = document.querySelector("Lower")
+
+let cardPull = () => {
+    let card = Math.random() * 13;
+    if(card = 11){
+        card = J
+    }
+    if (card = 12) {
+        card = Q
+    }
+    if ( card = 13){
+        card = K
+    } 
+    if ( card = 1){
+        card = A 
+    }
+
+}
+
+let accuracy = () => {
+streak = 0; 
+card(A) > ! card(A);
+
+if (card > card(random))
+streak = +1;
+}
+
+let playerPick = () => {
+    if (click higher) {
+        if (card > card(random))
+        streak = +1;
+    } else {
+        streak = 0;
+    }
+
+}
+
